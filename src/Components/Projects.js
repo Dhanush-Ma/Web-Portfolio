d043c1561cd6ed3720480053b482b494dffcbe07
Dhanush <dhanushmahesh.work@gmail.com>
2023-01-30 22:53:59 +0530

    made changes to Project Carousel Component

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,25 @@
 import styles from "../Styles/Projects.module.css";
-import Carousel from "react-elastic-carousel";
+import Carousel, { consts } from "react-elastic-carousel";
 import { projects } from "../data/project";
 import Project from "./Layouts/Project";
-
-
+import {useState} from 'react'
 
 const Projects = () => {
+  const [showArrow, setShowArrows] = useState(window.innerWidth)
+
+  window.addEventListener('resize', () => {
+    setShowArrows(window.innerWidth)
+  })
+
+  function myArrow({ type, onClick, isEdge }) {
+    const pointer = type === consts.PREV ? "" : "";
+    return (
+      <button onClick={onClick} disabled={isEdge}>
+        {pointer}
+      </button>
+    );
+  }
+
   const projectElemets = projects.map((project) => {
     return (
       <Project
@@ -24,7 +38,9 @@ const Projects = () => {
     <div className="container">
       <section id="projects" className={styles.projects}>
         <h1>Some of my Works</h1>
-        <Carousel itemsToShow={1}>{projectElemets}</Carousel>
+        <Carousel itemsToShow={1} renderArrow={showArrow < 700 && myArrow}>
+          {projectElemets}
+        </Carousel>
       </section>
     </div>
   );
