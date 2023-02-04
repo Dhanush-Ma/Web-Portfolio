9c4e5533533485202ed9ff8fb12367856282b629
Dhanush <dhanushmahesh.work@gmail.com>
2023-02-04 21:19:50 +0530

    made changes to progress  bar

--- src/Components/Layouts/Skill.js
+++ src/Components/Layouts/Skill.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Skill.module.css";
 import { motion } from "framer-motion";
 const Project = (props) => {
+  const [percentage, setPercentage] = useState(0);
+  const [progrssBar, setProgressBar] = useState({});
+
   const pageAnimate = {
     offScreen: { x: "-100", opacity: 0 },
     onScreen: {
@@ -14,12 +17,36 @@ const Project = (props) => {
       },
     },
   };
+
+  function animateProgressBar(finalValue) {
+    let progressValue = 0;
+    let speed = 20;
+    let progress = setInterval(() => {
+      progressValue++;
+      setPercentage(progressValue);
+      setProgressBar({
+        background: `conic-gradient(
+          var(--green) ${progressValue * 3.6}deg,
+          var(--ligter-green) ${progressValue * 3.6}deg
+        )`,
+      });
+
+      if (progressValue == finalValue) clearInterval(progress);
+    }, speed);
+  }
+
   return (
-    <motion.div className={styles.individualSkill} variants={pageAnimate}>
+    <motion.div
+      className={styles.individualSkill}
+      variants={pageAnimate}
+      onViewportEnter={() => animateProgressBar(props.percent)}
+    >
       <h3>{props.name}</h3>
       <div className={styles.skillWithPercent}>
         <i className={props.link}></i>
-        <p>{props.percent}</p>
+        <div className={styles.percentProgressBar} style={progrssBar}>
+          <p className={styles.percent}>{percentage}%</p>
+        </div>
       </div>
     </motion.div>
   );
