721a50d8cd544e77359b03ed4e72f581b54e7c6a
Dhanush-Ma <iamdhanush02@gmail.com>
2024-05-08 22:04:59 +0530

    chore: changed description

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,20 +19,30 @@ const About = () => {
         <div className={styles.aboutContainer}>
           <div>
             <p className={styles.desc}>
-              Penultimate year undergrad IT student who started Programming as a
-              complete Noob. I can undoubtedly say that I am a Self taught
-              Developer.I'm eager for internship opportunities to increase my
-              knowledge and enthusiasm for the software and web development
-              industry. The technology I've been surrounded by has been admiring
-              me for the past several years, and I hope my creativity combines
-              with technology to create a lovely environment for all of us.Right
-              now I am into Web Domain as it fascinated me at the start. I am
-              planning on to learn Mobile Development in the near feature. And
-              yes Googling is one of my top Skills.
+              As a penultimate year undergraduate IT student, I have carved a
+              niche for myself as a self-taught developer with a focus on
+              full-stack web and mobile application development. My freelance
+              journey has been rich and rewarding, having successfully managed
+              projects for over five clients, each with unique demands and
+              technological challenges. My expertise spans from designing
+              responsive user interfaces to developing complex backend
+              functionalities, using a blend of modern technologies that ensure
+              seamless user experiences. Currently, I'm deeply engrossed in the
+              web development domain, which first captured my interest and
+              continues to fuel my passion for coding. Looking ahead, I am eager
+              to expand my skill set into mobile development, aiming to craft
+              integrated solutions that bridge the gap between web and mobile
+              platforms.
             </p>
             <p>
-              Done an Internship for 2 weeks at Puducherry Technological
-              University as a team member in Controller of Examinations.
+              Research and problem-solving are integral parts of my workflow,
+              with 'Googling' serving as a vital skill in my arsenal, enabling
+              me to stay updated with the latest technologies and industry
+              practices. As I continue to grow professionally, I am enthusiastic
+              about exploring internship opportunities that will broaden my
+              horizons and help me contribute more effectively to the technology
+              sector, ultimately aiming to create an engaging and functional
+              digital environment for all users.
             </p>
             <p className={styles.current}>
               Currently I am Learning
