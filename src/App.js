e7d1b5a60e4917348b5b83166de7ee3f9933f57f
Dhanush Mahesh <102428471+Dhanush-Ma@users.noreply.github.com>
2025-06-17 16:12:53 +0530

    refacotor: rm linting code error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Header from "./Components/Header";
-import Quote from "./Components/Quote";
-import About from "./Components/About";
-import Skills from "./Components/Skills";
-import Projects from "./Components/Projects";
-import Contact from "./Components/Contact";
-import Footer from "./Components/Footer";
+// import Header from "./Components/Header";
+// import Quote from "./Components/Quote";
+// import About from "./Components/About";
+// import Skills from "./Components/Skills";
+// import Projects from "./Components/Projects";
+// import Contact from "./Components/Contact";
+// import Footer from "./Components/Footer";
 import { useEffect } from "react";
 
 function App() {
@@ -15,17 +15,17 @@ function App() {
 
   return null;
 
-  return (
-    <>
-      <Header />
-      <Quote />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Header />
+  //     <Quote />
+  //     <About />
+  //     <Skills />
+  //     <Projects />
+  //     <Contact />
+  //     <Footer />
+  //   </>
+  // );
 }
 
 export default App;
